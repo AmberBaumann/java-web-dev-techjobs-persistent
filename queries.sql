6b4597bf86dfa22aeb608ab2e605ce1b7c933ee0
AmberBaumann <ambermindsolliving@gmail.com>
2021-03-28 16:42:37 -0500

    Sooooo close to finishing

--- queries.sql
+++ queries.sql
@@ -10,5 +10,12 @@ FROM employer
 WHERE location="St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id is not null
+ORDER BY name ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,10 +1,13 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -13,12 +16,19 @@ public class Skill extends AbstractEntity {
     @NotBlank(message = "Skill description required.")
     private String description;
 
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
     public Skill(){}
